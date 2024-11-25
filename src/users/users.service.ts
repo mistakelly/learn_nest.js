a840c45b7c499412c4ddeff3f1da0b50baef5d2f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-25 20:34:49 +0100

    feat: Implement create user service for creating of user resource

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -26,5 +26,25 @@ export class UsersService {
 
     return singleUser;
   }
+  // Creates a new user and adds them to the UserDB
+  // The user is assigned an incremented ID
+  // @param body - the user data to create (from the CreateUserDto)
+  // @returns - the newly created user object
+  createUser(@Body() body: CreateUserDto): UserInterface {
+    // Generate a new user with an incremented ID
+    const user: UserInterface = {
+      id: UserDB[UserDB.length - 1]?.id + 1 || 1, // Increment ID based on the last user's ID
+      ...body, // Spread the body data (username, email, password)
+    };
 
+    // Add the new user to the UserDB
+    UserDB.push(user);
+
+    console.log('user length', UserDB.length); // Log the updated user list
+    console.log('new user', user); // Log the newly created user
+
+    return user; // Return the created user
+  }
+
+ 
 }
