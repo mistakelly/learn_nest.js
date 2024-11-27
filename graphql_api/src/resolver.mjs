c07289f81a96d5581885b415219f679d2ce36479
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-27 21:06:04 +0100

    feat: Implement postById resolver in order to fetch a single post

--- graphql_api/src/resolver.mjs
+++ graphql_api/src/resolver.mjs
@@ -5,20 +5,31 @@ import { Author } from './entities/authors.entity.mjs'
 
 const resolvers = {
     Query: {
+        // return Author array
         authors() {
             return Author
         },
 
+        // return a single author
+        authorById(_, args) {
+            console.log('args', args.id)
+            return Author.find(author => author.id == args.id)
+
+        },
+
+        // return Post array
         posts() {
             return Post
         },
 
-        authorById(_, args) {
-            console.log('args', args.id)
-            // return a single author
-            return Author.find(author => author.id == args.id)
+        // return single post
+        postById: (_, arg) => {
+            const { id } = arg
 
+            return Post.find(post => post.id == id)
         }
+
+
     }
 }
 
