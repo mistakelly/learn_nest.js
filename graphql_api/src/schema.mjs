168e6ebd9bfd2a2bdf273534376e41b111ef8ace
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-27 20:57:33 +0100

    Define Author and Post types, and add Query type for API schema modeling

---
+++ graphql_api/src/schema.mjs
@@ -0,0 +1,22 @@
+export const typeDefs = `#graphql
+  type Author {
+    id: ID!,
+    name: String!,
+    email: String,
+    gender: String!,
+  }
+
+  type Post {
+    id: ID,
+    title: String!,
+    content: String!,
+    createdAt: String!
+    authorId: Int!, 
+  }
+
+  type Query {
+    authors: [Author]
+    posts: [Post]
+    authorById(id: ID!): Author
+}
+`;
