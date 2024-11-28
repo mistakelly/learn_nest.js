b9a66d936f739af13924ba904c6723268c26d467
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 19:57:27 +0100

    Register UserEntity in user module

--- nest_graphql_api/src/users/users.module.ts
+++ nest_graphql_api/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UserResolver } from './resolvers/users.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/users.entity';
 
 @Module({
-  providers: [UsersService, UserResolver],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserResolver, UsersService],
 })
 export class UsersModule {}
