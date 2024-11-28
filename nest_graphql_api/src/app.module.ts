af408f51382e7c918b85e3b9eb22ffd2b9e6464d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 13:35:35 +0100

    feat: Configure GraphQL module

--- nest_graphql_api/src/app.module.ts
+++ nest_graphql_api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+
+// impot pluging for applo server
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      debug: true,
+      //   disable graphql playground
+      playground: false,
+
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    UsersModule,
+  ],
+
+  exports: [],
 })
 export class AppModule {}
