0ea7632b45778a6f13961cdf6f5040bcf8d77a3c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 15:49:21 +0100

    fix: Change hobbies option from text to 'simple-array' and removed array option cause mysql does not support array data-type

---
+++ nest_graphql_api/src/entities/users.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column('simple-array', { nullable: true })
+  hobbies: string[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
