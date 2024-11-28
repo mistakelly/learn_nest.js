074bfcd0771863e6a8983d8ae7a501c5400db9a9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 19:58:22 +0100

    Update users.model GraphQL Schema

--- nest_graphql_api/src/users/models/users.model.ts
+++ nest_graphql_api/src/users/models/users.model.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class UserModel {
@@ -10,4 +10,30 @@ export class UserModel {
 
   @Field()
   email: string;
+
+  @Field(() => [String], { nullable: true })
+  hobbies?: string[];
+
+  password: string;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  username: string;
+
+  @Field()
+  email: string;
+
+  @Field(() => [String], { nullable: true })
+  hobbies?: string[];
+
+  @Field()
+  password: string;
 }
