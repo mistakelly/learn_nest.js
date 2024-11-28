eadb8eee12399f7980f10a33f39868e508ce3984
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 19:56:18 +0100

    Implement UsersService with createUser and getAllUser methods

--- nest_graphql_api/src/users/services/users.service.ts
+++ nest_graphql_api/src/users/services/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from '../models/users.model';
+import { UserEntity } from 'src/entities/users.entity';
 
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+/**
+ * Service for user-related operations.
+ * It contains methods for creating a user and retrieving all users.
+ */
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  /**
+   * Creates a new user in the database.
+   * @param args - The user data input, including username, email, password, etc.
+   * @returns The created user entity with assigned id, createdAt, and updatedAt.
+   */
+  async createUser(args: CreateUserInput): Promise<UserEntity> {
+    const newUser = this.userRepository.create(args);
+    return this.userRepository.save(newUser);
+  }
+
+  /**
+   * Retrieves all users from the database.
+   * @returns A list of all user entities in the database.
+   */
+  async getAllUser() {
+    return await this.userRepository.find();
+  }
+}
