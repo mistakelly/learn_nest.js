b148f893be462fa1d8377de6cd3963eccdb031f3
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-28 20:00:28 +0100

    Update entities path

--- nest_graphql_api/src/app.module.ts
+++ nest_graphql_api/src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { UsersModule } from './users/users.module';
 
 // impot pluging for applo server
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// custom imports
+import { UsersModule } from './users/users.module';
+import AppDataSource from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // initialize database
+    TypeOrmModule.forRoot(AppDataSource),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
@@ -17,6 +27,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+
     UsersModule,
   ],
 
