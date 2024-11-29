66634f584d30a47129fa5836a149f193b4bf7686
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-29 22:58:49 +0100

    Export UserService and PasswordHashingService

---
+++ blog-graphql-api/src/modules/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UserResolver } from './resolvers/users.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/users.entity';
+import { PasswordHashingService } from './services/password_hashing.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserResolver, UsersService, PasswordHashingService],
+  exports: [UsersService, PasswordHashingService],
+})
+export class UsersModule {}
