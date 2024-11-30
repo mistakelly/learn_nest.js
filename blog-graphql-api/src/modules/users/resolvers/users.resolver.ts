1f237ebb2de8e18a7e8440e46323214cde412a5f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-11-30 03:42:24 +0100

    feat: Add a queries to get a user by Id and Email

--- blog-graphql-api/src/modules/users/resolvers/users.resolver.ts
+++ blog-graphql-api/src/modules/users/resolvers/users.resolver.ts
@@ -3,6 +3,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 // CUSTOM IMPORTS
 import { UserType } from 'src/shared/gql-types/user.type';
 import { UsersService } from '../services/users.service';
+import { BadRequestException } from '@nestjs/common';
 /**
  * Resolver for user-related GraphQL queries and mutations.
  * It connects the GraphQL schema with the service layer for user operations.
@@ -18,8 +19,34 @@ export class UserResolver {
    * @returns List of all users.
    */
 
+  // TODO: IMPLEMENT VALIDATION FOR VALID INPUT TYPES EG<EMPTY EMAIL INPUT>
+
+  @Query(() => UserType)
+  async findUserById(@Args('id') id: string) {
+    const user = await this.userService.findUserById(id);
+
+    if (!user) {
+      throw new BadRequestException('user Not found');
+    }
+
+    return user;
+  }
+
+  @Query(() => UserType)
+  async findUserByEmail(@Args('id') id: string) {
+    const user = await this.userService.findUserByEmail(id);
+
+    if (!user) {
+      throw new BadRequestException('user Not found');
+    }
+
+    return user;
+  }
+
   @Query(() => [UserType])
-  async getAllUser(): Promise<UserType[]> {
+  async findUsers(): Promise<UserType[]> {
     return this.userService.getAllUser();
   }
+
+  
 }
